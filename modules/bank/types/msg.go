1494a7e4bcecd76fa70af9d0752576ec1dbebcb3
baymax19 <baymax19@protonmail.com>
2019-10-16 17:47:01 +0530

    Adding Msg

--- modules/bank/types/msg.go
+++ modules/bank/types/msg.go
@@ -40,3 +40,39 @@ func (MsgUser) Type() string {
 func (msg MsgUser) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.Signer}
 }
+
+type MsgTokenTransfer struct {
+	ChannelID string
+	Signer    sdk.AccAddress
+	ToAddress sdk.AccAddress
+	Coins     sdk.Coins
+}
+
+var _ sdk.Msg = MsgTokenTransfer{}
+
+func NewMsgTokenTransfer(name, chanID string, from, to sdk.AccAddress, coins sdk.Coins) MsgTokenTransfer {
+	return MsgTokenTransfer{
+		ChannelID: chanID,
+		Signer:    from,
+		ToAddress: to,
+		Coins:     coins,
+	}
+}
+
+func (msg MsgTokenTransfer) ValidateBasic() sdk.Error { return nil }
+
+func (msg MsgTokenTransfer) GetSignBytes() []byte {
+	return sdk.MustSortJSON(cdc.MustMarshalJSON(msg))
+}
+
+func (MsgTokenTransfer) Route() string {
+	return "send"
+}
+
+func (MsgTokenTransfer) Type() string {
+	return "user"
+}
+
+func (msg MsgTokenTransfer) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Signer}
+}
